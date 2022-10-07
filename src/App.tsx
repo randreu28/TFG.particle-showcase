3bc7b79239eebfb0f438e789728477de72bcd474
randreu28 <randreu28@gmail.com>
2022-10-07 13:34:25 +0200

    Adds r3f stack dependencies
    Creates a simple <Canvas />

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useControls } from "leva";
+
 export default function App() {
+  const { color, scale } = useControls({
+    color: "#ff005b",
+    scale: [1, 1, 1],
+  });
+
   return (
-    <div className="h-screen flex">
-      <div className="m-auto text-5xl">Hello world</div>
+    <div className="h-screen bg-gray-600">
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={0.1} />
+        <directionalLight color={color} position={[1, 2, 5]} />
+        <directionalLight color={color} position={[-1, -2, -5]} />
+        <mesh scale={scale}>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
