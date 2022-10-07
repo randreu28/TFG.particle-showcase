4a31f4f0021b260d139311943edfb1d68939e261
randreu28 <randreu28@gmail.com>
2022-10-07 13:55:21 +0200

    Adds Vignette postprocessing effect to bg

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Vignette, EffectComposer } from "@react-three/postprocessing";
 import { useControls } from "leva";
 
 export default function App() {
-  const { color, scale } = useControls({
+  const { color, scale, backgroundColor } = useControls({
     color: "#ff005b",
     scale: [1, 1, 1],
+    backgroundColor: "#ffffff",
   });
 
   return (
-    <div className="h-screen bg-gray-600">
+    <div className="h-screen">
       <Canvas>
+        <color attach="background" args={[backgroundColor]} />
+        <EffectComposer>
+          <Vignette eskil={true} opacity={0.5} offset={0.1} darkness={1.5} />
+        </EffectComposer>
         <OrbitControls />
         <ambientLight intensity={0.1} />
         <directionalLight color={color} position={[1, 2, 5]} />
