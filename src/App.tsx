434197b0078c6b3eb48ccdcf48f29839be510ce4
randreu28 <randreu28@gmail.com>
2022-10-10 11:07:07 +0200

    Updates Scene params
    Removes the lights and adds a vignette paramater

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import { useControls } from "leva";
 import Buffer from "./Buffer";
 
 export default function App() {
-  const { lightColor, backgroundColor } = useControls("Scene", {
-    lightColor: "#ffffff",
+  const { vignette, backgroundColor } = useControls("Scene", {
+    vignette: { value: 0.5, min: 0, max: 1 },
     backgroundColor: "#191a2e",
   });
 
@@ -17,10 +17,14 @@ export default function App() {
         <PerspectiveCamera makeDefault position={[3, 2, 3]} />
         <color attach="background" args={[backgroundColor]} />
         <EffectComposer>
-          <Vignette eskil={true} opacity={0.5} offset={0.1} darkness={1.5} />
+          <Vignette
+            eskil={true}
+            opacity={vignette}
+            offset={0.1}
+            darkness={1.5}
+          />
         </EffectComposer>
         <OrbitControls />
-        <ambientLight color={lightColor} intensity={0.1} />
         <Buffer />
       </Canvas>
     </div>
