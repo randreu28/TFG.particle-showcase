90ba656d2905e030bb854d6d566e549bf1f924d4
randreu28 <randreu28@gmail.com>
2022-10-11 11:34:45 +0200

    Cycles between text scrambles according to tick

--- src/DynamicText.tsx
+++ src/DynamicText.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { TextScramble } from "@a7sc11u/scramble";
+import useStore from "./store/store";
 
 type Props = {};
 
 export default function DynamicText({}: Props) {
+  let tick = useStore((state) => state.tick);
+
+  const text: string[] = [
+    "Lorem ipsum (0)",
+    "Lorem ipsum (1)",
+    "Lorem ipsum (2)",
+    "Lorem ipsum (3)",
+  ];
+
   return (
     <TextScramble
-      className="text-xl"
-      as="h1"
-      text="Lorem ipsum siamet dolorum"
+      className="bg-[#ef079b] bg-gradient-to-br text-transparent from-[#f8665d] via-[#f8665d] bg-clip-text"
+      as="span"
+      text={text[tick]}
     />
   );
 }
